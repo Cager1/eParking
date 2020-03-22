34a01753f29cc6b2b91772e860f732e2228006f5
Filip Crnogorac <38707886+Cager1@users.noreply.github.com>
2020-03-22 23:24:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ fetch('http://smart.sum.ba/parking?withParkingSpaces=1', {
 		}
 	})
 
+
+var i = 0;
+while (i < 100) {
+ 	(function(i) {
+    	setTimeout(function() {
+    		fetch('http://smart.sum.ba/parking?withParkingSpaces=1', {
+		})
+			.then(res => res.json())
+			.then(data => {
+				var occu = [];
+				for (var i = 1; i < 63; i++) {
+					var value = data[0].parkingSpaces[i-1].occupied;
+					occu.push(value);
+				}
+
+				for (var i = 1; i < occu.length + 1; i++) {
+					if (occu[i - 1] == 1) {
+						document.getElementById(i).style.backgroundColor = "red";
+					} else {
+						document.getElementById(i).style.backgroundColor = "green";
+					}
+				}
+			})
+    	}, 1000 * i)
+    })(i++)
+  }
+
