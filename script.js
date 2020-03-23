fbc3d23d28b8426876d399064b73aeb04bbc0c72
Filip Crnogorac <38707886+Cager1@users.noreply.github.com>
2020-03-23 17:25:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,29 +19,20 @@ fetch('http://smart.sum.ba/parking?withParkingSpaces=1', {
 	})
 
 
-var i = 0;
-while (i < 100000) {
- 	(function(i) {
-    	setTimeout(function() {
-    		fetch('http://smart.sum.ba/parking?withParkingSpaces=1', {
-		})
-			.then(res => res.json())
-			.then(data => {
-				var occu = [];
-				for (var i = 1; i < 63; i++) {
-					var value = data[0].parkingSpaces[i-1].occupied;
-					occu.push(value);
-				}
+var socket = io.connect('http://smart.sum.ba/parking-events')
 
-				for (var i = 1; i < occu.length + 1; i++) {
-					if (occu[i - 1] == 1) {
-						document.getElementById(i).style.backgroundColor = "red";
-					} else {
-						document.getElementById(i).style.backgroundColor = "green";
-					}
-				}
-			})
-    	}, 1000 * i)
-    })(i++)
-  }
+
+socket.on('parking-lot-state-change', function(data) {
+	console.log(data)
+	console.log(data.id_parking_space);
+	console.log("State change on " + data.id_parking_space + " parking spot");
+	if (data.occupied == 1) {
+		document.getElementById(data.id_parking_space).style.backgroundColor = "red";
+		document.getElementById(data.id_parking_space).innerHTML = "Red";
+	} else {
+		document.getElementById(data.id_parking_space).style.backgroundColor = "green";
+		document.getElementById(data.id_parking_space).innerHTML = "Green";
+	}
+})
+
 
