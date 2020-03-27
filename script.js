c36093f28b513b75a4019d92736c523c3a3dd226
Filip Crnogorac <38707886+Cager1@users.noreply.github.com>
2020-03-27 01:00:26 +0100

    Update script.js
    Ubacio auta

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 fetch('http://smart.sum.ba/parking?withParkingSpaces=1', {
 })
 	.then(res => res.json())
@@ -11,12 +12,25 @@ fetch('http://smart.sum.ba/parking?withParkingSpaces=1', {
 		for (var i = 1; i < occu.length + 1; i++) {
 			if (occu[i - 1] == 1) {
 				document.getElementById(i).style.backgroundColor = "red";
+				if (i >= 1 && i <= 13) {
+					//top
+					document.getElementById(i).classList.add("carUp");
+				}else if (i >= 14 && i <= 20) {
+					//right
+					document.getElementById(i).classList.add("carRight");
+				}else if (i >= 21 && i <= 27) {
+					//left
+					document.getElementById(i).classList.add("carLeft");
+				}else if (i >= 28 && i <= 62) {
+					//right
+					document.getElementById(i).classList.add("carRight");
+				}
+
 			} else {
 				document.getElementById(i).style.backgroundColor = "green";
 			}
 		}
 	})
-
 
 var socket = io.connect('http://smart.sum.ba/parking-events');
 
@@ -27,8 +41,36 @@ socket.on('parking-lot-state-change', function(data) {
 	console.log("State change on " + data.id_parking_space + " parking spot");
 	if (data.occupied == 1) {
 		document.getElementById(data.id_parking_space).style.backgroundColor = "red";
+		if (data.id_parking_space >= 1 && data.id_parking_space <= 13) {
+			//top
+			document.getElementById(data.id_parking_space).classList.add("carUp");
+		}else if (data.id_parking_space >= 14 && data.id_parking_space <= 20) {
+			//right
+			document.getElementById(data.id_parking_space).classList.add("carRight");
+		}else if (data.id_parking_space >= 21 && data.id_parking_space <= 27) {
+			//left
+			document.getElementById(data.id_parking_space).classList.add("carLeft");
+		}else if (data.id_parking_space >= 28 && data.id_parking_space <= 62) {
+			//right
+			document.getElementById(data.id_parking_space).classList.add("carRight");
+		}
+
 	} else {
 		document.getElementById(data.id_parking_space).style.backgroundColor = "green";
+
+		if (data.id_parking_space >= 1 && data.id_parking_space <= 13) {
+			//top
+			document.getElementById(data.id_parking_space).classList.remove("carUp");
+		}else if (data.id_parking_space >= 14 && data.id_parking_space <= 20) {
+			//right
+			document.getElementById(data.id_parking_space).classList.remove("carRight");
+		}else if (data.id_parking_space >= 21 && data.id_parking_space <= 27) {
+			//left
+			document.getElementById(data.id_parking_space).classList.remove("carLeft");
+		}else if (data.id_parking_space >= 28 && data.id_parking_space <= 62) {
+			//right
+			document.getElementById(data.id_parking_space).classList.remove("carRight");
+		}
 	}
 });
 
