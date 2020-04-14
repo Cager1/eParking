91ba28fe7e44a513b809f5c8f012e0441e369e1a
Mario12335 <51788714+Mario12335@users.noreply.github.com>
2020-04-14 18:49:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+window.scrollTo(0, 50000);
 fetch('http://smart.sum.ba/parking?withParkingSpaces=1', {
 })
 	.then(res => res.json())
@@ -75,32 +75,4 @@ socket.on('parking-lot-state-change', function(data) {
 });
 
 
-(function() {
-	scrollTo1();
-})();
 
-function scrollTo1() {
-	const links = document.querySelectorAll('.scroll');
-	links.forEach(each => (each.onclick = scrollAnchors));
-}
-
-function scrollAnchors(e, respond = null) {
-	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-	e.preventDefault();
-	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-	const targetAnchor = document.querySelector(targetID);
-	if (!targetAnchor) return;
-	const originalTop = distanceToTop(targetAnchor);
-	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-	const checkIfDone = setInterval(function() {
-		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-		if (distanceToTop(targetAnchor) === 0 || atBottom) {
-			targetAnchor.tabIndex = '-1';
-			targetAnchor.focus();
-			window.history.pushState('', '', targetID);
-			clearInterval(checkIfDone);
-		}
-	}, 100);
-}
-
-window.scrollTo(0,100000000);
